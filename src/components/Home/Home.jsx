8ae24760e6db8dedd3fd2b437a205de2229d5d4b
ibarrajon <jon.programador@hotmail.es>
2023-09-08 15:18:06 -0300

    funcionalidad de envio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Home.css'
+import emailjs from '@emailjs/browser';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const togglePopup = () => setIsOpen(!isOpen);
+    
+    const [isEnviado, setIsEnviado] = useState(false);
+    const toggleEnviado = () => setIsEnviado(!isEnviado);
 
-    const togglePopup = () => {
-        setIsOpen(!isOpen);
-    };
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_7mr5h09', 'template_6m83kyl', form.current, 'H_mOvcyS7nF4y0zOk')
+          .then((result) => {
+              console.log("Enviado:",result.text);
+              toggleEnviado();
+              togglePopup();
+          }, (error) => {
+              console.log("Error:",error.text);
+          });
+      };
 
     return (
         <div className='container'>
@@ -16,28 +32,28 @@ const Home = () => {
             {isOpen && (
                 <div className="popup">
                     
-                        <form>
+                        <form ref={form} onSubmit={sendEmail}>
                             <div className="form-container">
                                 <label>Nombre:</label>
-                                <input className='contacto-input' type="text" id="nombre" required />
+                                <input className='contacto-input' type="text" name="user_name" required />
 
                                 <label>Email:</label>
-                                <input className='contacto-input' type="email" id="email" required />
+                                <input className='contacto-input' type="email" name="user_email" required />
 
                                 <label>Asunto:</label>
-                                <input className='contacto-input' type="text" id="asunto" required />
+                                <input className='contacto-input' type="text" name="user_asunto" required />
 
                                 <label>Mensaje:</label>
-                                <textarea className='contacto-mensaje' id="message"></textarea>
+                                <textarea className='contacto-mensaje' name="message"></textarea>
 
                                 <div className="enviar-container">
                                     <button className='enviar-form' type="submit">Enviar</button>
                                     <button className='enviar-form' onClick={togglePopup}>Cerrar</button>
                                 </div>
+                                <p className='mensaje-enviado'>{isEnviado ? 'Mensaje enviado correctamente' : ''}</p>
                             </div>
                         </form>
-                 
-                </div>
+                </div> 
             )}
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae doloribus quaerat fuga suscipit amet odit assumenda velit voluptas dolorem mollitia iste, sint reiciendis excepturi quidem aspernatur tenetur magnam dolores quas.
